b0384531c70187867358a29c0367c47dfcf4e62c
anhthanh260599@gmail.com <anhthanh260599@gmail.com>
2023-06-08 20:41:50 +0700

    a

---
+++ QuanLyKhoaHoc/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using QuanLyKhoaHoc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuanLyKhoaHoc.Controllers
+{
+    public class HomeController : Controller
+    {
+        VNR_InternShipEntities database = new VNR_InternShipEntities();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult FrontEnd()
+        {
+            return View(database.MonHocs.Where(s=>s.KhoaHocID == 1).ToList());
+        }
+
+        public ActionResult BackEnd()
+        {
+            return View(database.MonHocs.Where(s => s.KhoaHocID == 2).ToList());
+        }
+
+        public ActionResult DotnetCore()
+        {
+            return View(database.MonHocs.Where(s => s.KhoaHocID == 3).ToList());
+        }
+    }
+}
